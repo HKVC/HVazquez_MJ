957c8fa89eead099cd8c8e8c57c5635b90925aa2
Harry Vazquez <harrykvazquezcolon@gmail.com>
2024-03-05 10:06:42 -0400

    b

---
+++ C#/LPC#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        int myVariable = 10;
+        const int myConstant = 20;
+
+        Console.WriteLine("Valor de mi variable:" + myVariable);
+
+        Console.WriteLine("Valor de mi constante:" + myConstant); 
+    }
+}
+
--- Java/HelloWorld/src/App.java
+++ Java/HelloWorld/src/App.java
@@ -3,5 +3,9 @@ public class App
     public static void main (String[] args) throws Exception
     {
         System.out.println("Ikou Z!");
+
+        System = 99;
+
+        System.out.println("Lmao" + System);
     }
 }
